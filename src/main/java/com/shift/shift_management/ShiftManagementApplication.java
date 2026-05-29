4053601c9d43bdd174848a960b7283e139591c4c
Daiki Onaga <253111@ocsjoho.onmicrosoft.com>
2026-05-29 15:31:05 +0900

    initial setup

---
+++ src/main/java/com/shift/shift_management/ShiftManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shift.shift_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shift/shift_management/ShiftManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shift.shift_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
