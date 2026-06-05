7ebe7c4e7afe058fa15c8cd985311ec513ac5402
Daiki Onaga <253111@ocsjoho.onmicrosoft.com>
2026-06-05 21:50:42 +0900

    add cors config

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,10 @@ import { useEffect, useState } from "react";
 import api from "../api/api";
 
 function Home() {
-
     const [period, setPeriod] = useState(null);
+    const [error, setError] = useState("");
 
     useEffect(() => {
-
         api.get("/api/submission-periods/current")
             .then(response => {
                 console.log(response.data);
@@ -14,30 +13,24 @@ function Home() {
             })
             .catch(error => {
                 console.log(error);
+                setError("API取得に失敗しました");
             });
-
     }, []);
 
     return (
         <div>
             <h1>シフト管理アプリ</h1>
 
+            {error && <p>{error}</p>}
+
             {period ? (
                 <>
-                    <p>
-                        {period.startDate}
-                        〜
-                        {period.endDate}
-                    </p>
-
-                    <p>
-                        締切：{period.deadline}
-                    </p>
+                    <p>{period.startDate}〜{period.endDate}</p>
+                    <p>締切：{period.deadline}</p>
                 </>
             ) : (
-                <p>読み込み中...</p>
+                !error && <p>読み込み中...</p>
             )}
-
         </div>
     );
 }
