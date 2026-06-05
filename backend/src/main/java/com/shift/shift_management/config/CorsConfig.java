e5b3e6f4396fc79dc5b977ccca7b19043f499766
Daiki Onaga <253111@ocsjoho.onmicrosoft.com>
2026-06-06 00:19:38 +0900

    Vercel導入

--- backend/src/main/java/com/shift/shift_management/config/CorsConfig.java
+++ backend/src/main/java/com/shift/shift_management/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "http://shift-management-navy.vercel.app"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
