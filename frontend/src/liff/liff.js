9bb82c6443ba501811110bb63782fc5affc5cc58
Daiki Onaga <253111@ocsjoho.onmicrosoft.com>
2026-06-07 19:33:41 +0900

    LIFF導入

---
+++ frontend/src/liff/liff.js
@@ -0,0 +1,33 @@
+import liff from "@line/liff";
+
+export const initLiff = async () => {
+
+  console.log("LIFF開始");
+
+  const liffId = import.meta.env.VITE_LIFF_ID;
+
+  console.log("LIFF ID:", liffId);
+
+  await liff.init({
+    liffId: liffId,
+  });
+
+  console.log("LIFF初期化成功");
+
+  console.log("ログイン状態:", liff.isLoggedIn());
+
+  if (!liff.isLoggedIn()) {
+    liff.login();
+    return null;
+  }
+
+  const profile = await liff.getProfile();
+
+  console.log("プロフィール:", profile);
+
+  return {
+    lineUserId: profile.userId,
+    displayName: profile.displayName,
+    pictureUrl: profile.pictureUrl,
+  };
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
 import Layout from "../components/Layout";
+import { useEffect, useState } from "react";
+import { initLiff } from "../liff/liff";
 
 function Home() {
+  const [lineUser, setLineUser] = useState(null);
+
+  useEffect(() => {
+    initLiff()
+      .then((user) => {
+        console.log("LINE user:", user);
+        setLineUser(user);
+      })
+      .catch((error) => {
+        console.error("LIFF error:", error);
+      });
+  }, []);
+
   return (
     <Layout title="シフト管理アプリ">
+
+      {lineUser && (
+        <div>
+          <p>LINE名：{lineUser.displayName}</p>
+          <p>ID：{lineUser.lineUserId}</p>
+        </div>
+      )}
+
       <div className="menu">
         <Link to="/submit" className="menu-button">
           シフト提出
@@ -27,6 +50,7 @@ function Home() {
           管理者：確定シフト作成
         </Link>
       </div>
+
     </Layout>
   );
 }
