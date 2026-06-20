2e7ad4e04c727807bd2af2a9ddae3b73d64b063d
Daiki Onaga <253111@ocsjoho.onmicrosoft.com>
2026-06-20 20:27:51 +0900

    import忘れ

--- backend/src/main/java/com/shift/shift_management/repository/ShiftRequestRepository.java
+++ backend/src/main/java/com/shift/shift_management/repository/ShiftRequestRepository.java
@@ -2,6 +2,8 @@ package com.shift.shift_management.repository;
 
 import com.shift.shift_management.entity.ShiftRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
+import java.util.Optional;
 import java.util.List;
 
 public interface ShiftRequestRepository extends JpaRepository<ShiftRequest, Long> {
